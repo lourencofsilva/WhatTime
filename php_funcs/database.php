f57b076c6b5adacc8299c248ded7dc74afa41921
aran <07acampbell@brightoncollege.net>
2023-03-09 13:04:24 +0000

    D/T formatting

--- php_funcs/database.php
+++ php_funcs/database.php
@@ -436,7 +436,7 @@ function createGroupLink($groupName): string {
 function getUserEvents($user_id){
     $pdo = openConn();
 
-    $sql = "SELECT active, summary, DATE_FORMAT(dt_start, '%Y-%m-%dT%H:%i:%s') as dt_start, DATE_FORMAT(dt_end, '%Y-%m-%eT%H:%i:%s') as dt_end
+    $sql = "SELECT summary, DATE_FORMAT(dt_start, '%Y-%m-%dT%H:%i:%s') as start, DATE_FORMAT(dt_end, '%Y-%m-%eT%H:%i:%s') as 'end'
             FROM events
             WHERE user_id = :user_id AND active = 1";
     $stmt = $pdo->prepare($sql);
@@ -460,7 +460,7 @@ function getUserEvents($user_id){
 
 function getGroupEvents($group_id){
     $pdo = openConn();
-    $sql = "SELECT 'UNAVAILABLE' as title, DATE_FORMAT(dt_start, '%Y-%m-%dT%H:%i:%s') as dt_start, DATE_FORMAT(dt_end, '%Y-%m-%eT%H:%i:%s') as dt_end
+    $sql = "SELECT 'UNAVAILABLE' as title, DATE_FORMAT(dt_start, '%Y-%m-%dT%H:%i:%s') as start, DATE_FORMAT(dt_end, '%Y-%m-%eT%H:%i:%s') as 'end'
             FROM (events INNER JOIN user_group_link ON events.user_id = user_group_link.user_id) 
             WHERE user_group_link.group_id=:group_id AND events.active = 1";
     $stmt = $pdo->prepare($sql);
