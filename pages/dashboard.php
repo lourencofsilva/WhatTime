0dc42dfd85c133e85a6579e237505582ba4b3a85
Lourenco Figueiredo Silva <lourenco.figueiredosilva@student.manchester.ac.uk>
2023-03-18 00:40:03 +0000

    Refresh fixes

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -64,7 +64,6 @@ if (!empty($groups)) {
 	<link rel="stylesheet" type="text/css" href="../css/modal.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<script defer type="text/javascript" src="../js/createGroupModal.js"></script>
-	<script defer type="text/javascript" src="../js/manageGroupModal.js"></script>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
@@ -237,7 +236,7 @@ if (!empty($groups)) {
         // Function for searching group names
 	    function search() {
 	        	let text = document.getElementById("search").value.toLowerCase();
-	        	$('.member-row').each(function(i, obj) {
+	        	$('.group_row').each(function(i, obj) {
 	        		var name = document.getElementsByClassName('group_name_container')[i].innerHTML.toLowerCase();
 	        		if (name.includes(text)) {
 	        			obj.style.display = "block";
