619c0008e8c6a202ae43a7d6e82a97141575ad12
Lourenco Figueiredo Silva <lourenco.figueiredosilva@student.manchester.ac.uk>
2023-03-22 12:53:54 +0000

    Revert test for viewport

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -52,6 +52,7 @@ if (!empty($groups)) {
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Dashboard · WhatTime?</title>
 	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
 	<meta http-equiv="Pragma" content="no-cache" />
